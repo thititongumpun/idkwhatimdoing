68056bc7287ee7034519305df02220bb5ed22837
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-04-01 20:59:00 +0700

    add toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
+import React, { useState } from 'react';
 import { TodoListItem } from "./components/TodoListItem";
 
-const todos: Todo[] = [{text: 'run', complete: false}, {text: 'walk', complete: true}]
+const initialTodos: Todo[] = [
+  {
+    text: 'run',
+    complete: false
+  },
+  {
+    text: 'walk'
+    , complete: true
+  }
+];
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
 
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  }
   return (
-    <div className="App">
+    <ul className="App">
       <h3>Hello let begin Agian!?</h3>
-      <TodoListItem todo={todos[0]} />
-      <TodoListItem todo={todos[1]} />
-    </div>
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
+    </ul>
   );
 }
 
