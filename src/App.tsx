0052bba92fe010a8e891a25f921eca7a74324f0f
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-04-01 20:45:42 +0700

    remove necessary thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-
-import { Heading } from "./components/Heading";
-import {Label} from './components/Label';
 import { TodoListItem } from "./components/TodoListItem";
 
 const todos: Todo[] = [{text: 'run', complete: false}, {text: 'walk', complete: true}]
@@ -10,9 +7,8 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <h3>Hello let begin Agian!?</h3>
-      <Heading title="Hello Props from Heading" />
-      <Label title="sada" content="sadsa" />
-      <TodoListItem todo={todos[0]}/>
+      <TodoListItem todo={todos[0]} />
+      <TodoListItem todo={todos[1]} />
     </div>
   );
 }
